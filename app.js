9174873f58808ca47d1c0f26d94a945799c3ade2
shibinragh1 <shibinragh@gmail.com>
2018-08-02 00:44:06 +0530

    post Done

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const product = require('./routes/product.route');
 // initialize our express app
 const app = express();
+
+
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://product:password123@ds263791.mlab.com:63791/productstutorial';
+//'mongodb://shibinragh1:password123@ds147391.mlab.com:47391/ask-questions'
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/products', product);
+
+
+
+
+
+
+
+
+
 
 let port = 1234;
 
