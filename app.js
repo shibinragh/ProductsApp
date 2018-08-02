01b9e8697623bef96d0b2c9fc39d6c59f227ce17
shibinragh <shibin@toobler.com>
2018-08-02 17:30:36 +0530

    completed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://product:password123@ds263791.mlab.com:63791/productstutorial';
+let dev_db_url = 'mongodb://product:password123@ds263791.mlab.com:63791/productstutorial';  
 //'mongodb://shibinragh1:password123@ds147391.mlab.com:47391/ask-questions'
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
@@ -19,12 +19,6 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
-
-
-
-
-
-
 
 
 
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,17 +4,7 @@ exports.test = function (req, res){
     res.send('Greetings from the Test controller!')
 }
 
-/*exports.product_create = function(req, res){
-    let product = new Product({
-        name: req.body.name,
-        price: req.body.price
-    });
-    product.save(function(err){
-        if(err) return next(err);
-        
-        res.send('Product Created successfully');
-    })
-};*/
+
 exports.product_create = function (req, res) {
     let product = new Product(
         {
@@ -28,5 +18,26 @@ exports.product_create = function (req, res) {
             return next(err);
         }
         res.send('Product Created successfully')
+    })
+};
+
+exports.product_details = function(req, res){
+    Product.findById(req.params.id, function(err,product){
+        if (err) return next(err)
+        res.send(product);
+    })
+};
+
+exports.product_update = (req, res) =>{
+    Product.findByIdAndUpdate(req.params.id,{ $set: req.body}, function(err, product){
+        if(err) return next(err);
+        res.send('Product udpated.');
+    })
+};
+
+exports.product_delete = (req, res) =>{
+    Product.findByIdAndRemove(req.params.id, function(err){
+        if(err) return next(err);
+        res.send('Product Deleted.'); 
     })
 };
